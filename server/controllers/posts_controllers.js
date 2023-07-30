8b42033e865e0c7c8a787ac39e809e2df68075ff
Kevin Condezo <86019347+KevinC2V@users.noreply.github.com>
2023-07-30 11:27:31 -0500

    add getAllPosts controller

--- server/controllers/posts_controllers.js
+++ server/controllers/posts_controllers.js
@@ -32,3 +32,15 @@ export const createPost = async (req, res) => {
     res.status(409).json({ message: err.message });
   }
 };
+
+export const getAllPosts = async (req, res) => {
+  try {
+    // Se filtran los campos más importantes
+    const all_posts = await Post.find().select(
+      "city price room_type capacity tags photos_url"
+    );
+    res.status(201).json(all_posts);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
--- server/routes/posts_routes.js
+++ server/routes/posts_routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import {
-  /* getAllPosts, getPost, */ createPost,
+  /* getPost, */ createPost,
+  getAllPosts,
 } from "../controllers/posts_controllers.js";
 
 const posts_router = express.Router();
 
-// posts_router.get("/", getAllPosts);
+posts_router.get("/", getAllPosts);
 // posts_router.get("/:post_id", getPost);
 posts_router.post("/new", createPost);
 

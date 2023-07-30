dff168c10e281278547e22476378fc4ba69eb01e
Kevin Condezo <86019347+KevinC2V@users.noreply.github.com>
2023-07-29 23:38:58 -0500

    corrige atributos de Post

--- server/models/posts.js
+++ server/models/posts.js
@@ -2,32 +2,32 @@ import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema(
   {
-    direccion: {
+    address: {
       type: String,
       required: true,
     },
-    ciudad: {
+    city: {
       type: String,
       required: true,
     },
-    precio: {
+    price: {
       type: Number,
       get: (v) => (v / 100).toFixed(2),
       set: (v) => v * 100,
     },
-    tipo: {
+    room_type: {
       // "Cuarto", "Departamento", "Casa"
       type: String,
       required: true,
     },
-    caracteristicas: {
-      // array de tags: ["Baño propio", "Elevador", "Parque"]
+    tags: {
+      // array de características: ["Baño propio", "Elevador", "Parque"]
       type: [String],
       default: [],
     },
-    descripcion: String,
-    nro_roomies: Number,
-    url_fotos: {
+    description: String,
+    capacity: Number, // número de roomies
+    photos_url: {
       // arreglo de url de las fotos del post
       type: [String],
       default: [],

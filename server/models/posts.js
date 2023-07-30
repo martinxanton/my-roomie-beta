3a0b00ed3fd587d1200e1dfaa7e6a7a84cf3e68a
Kevin Condezo <86019347+KevinC2V@users.noreply.github.com>
2023-07-30 11:11:43 -0500

    update Post model constraints

--- server/models/posts.js
+++ server/models/posts.js
@@ -4,33 +4,43 @@ const postSchema = new mongoose.Schema(
   {
     address: {
       type: String,
-      required: true,
+      required: [true, "Address is required"],
     },
     city: {
       type: String,
-      required: true,
+      required: [true, "City is required"],
     },
     price: {
       type: Number,
+      required: [true, "Price is required"],
       get: (v) => (v / 100).toFixed(2),
       set: (v) => v * 100,
     },
     room_type: {
       // "Cuarto", "Departamento", "Casa"
       type: String,
-      required: true,
+      required: [true, "Room type is required"],
+    },
+    capacity: {
+      // número de roomies
+      type: Number,
+      required: [true, "Capacity is required"],
     },
     tags: {
       // array de características: ["Baño propio", "Elevador", "Parque"]
       type: [String],
       default: [],
     },
-    description: String,
-    capacity: Number, // número de roomies
+    description: {
+      type: String,
+      default: "",
+    },
     photos_url: {
       // arreglo de url de las fotos del post
       type: [String],
-      default: [],
+      default: [
+        "https://images.freeimages.com/images/large-previews/eae/roomz-5-1231926.jpg",
+      ],
     },
   },
   {
